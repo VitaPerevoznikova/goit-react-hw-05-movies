4155de59b0101cb408629d4e75405ba41003d1c0
VitaPerevoznikova <frayvita@gmail.com>
2023-11-09 23:05:10 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import TrendingMovie from '../components/api/api';
 import { onHandingError } from 'components/api/error_handling';
-import MovieList from '../components/MovieList/MovieList';
+import MovieList from 'components/MovieList/MovieList';
 import Loader from 'components/Loader/Loader';
 
 const Home = () => {
