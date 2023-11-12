4585ac07083b33eb119925e2af650fab7092f7bf
VitaPerevoznikova <frayvita@gmail.com>
2023-11-12 13:53:02 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,38 +1,46 @@
-import { GetReviewsMovie } from "components/api/api";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { ReviewsDescr, ReviewsItem, ReviewsList } from "./Reviews.style";
+import { GetReviewsMovie } from 'components/api/api';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ReviewsDescr, ReviewsItem, ReviewsList } from './Reviews.style';
+import Loader from 'components/Loader/Loader';
 
 const Reviews = () => {
-    const { movieId } = useParams();
-    const [reviews, setReviews] = useState([]);
-  
-    useEffect(() => {
-      const fetchReviews = async () => {
-        try {
-          const Reviews = await GetReviewsMovie(movieId);
-          setReviews(Reviews);
-        } catch (e) {
-          console.log(e);
-        }
-      };
-      fetchReviews();
-    }, [movieId]);
-  
-    return reviews.length === 0 ? (
-      <ReviewsDescr>No Reviews.</ReviewsDescr>
-    ) : (
-      <ReviewsList>
-        {reviews.map(({ id, author, content }) => (
-          <ReviewsItem key={id}>
-            <p>
-              <span>Author:</span> {author}
-            </p>
-            <ReviewsDescr>{content}</ReviewsDescr>
-          </ReviewsItem>
-        ))}
-      </ReviewsList>
-    );
-  };
-  
-  export default Reviews;
+  const { movieId } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchReviews = async () => {
+      try {
+        const Reviews = await GetReviewsMovie(movieId);
+        setReviews(Reviews);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchReviews();
+  }, [movieId]);
+  if (loading) {
+    return <Loader />;
+  }
+
+  return reviews.length === 0 ? (
+    <ReviewsDescr>No Reviews.</ReviewsDescr>
+  ) : (
+    <ReviewsList>
+      {reviews.map(({ id, author, content }) => (
+        <ReviewsItem key={id}>
+          <p>
+            <span>Author:</span> {author}
+          </p>
+          <ReviewsDescr>{content}</ReviewsDescr>
+        </ReviewsItem>
+      ))}
+    </ReviewsList>
+  );
+};
+
+export default Reviews;
